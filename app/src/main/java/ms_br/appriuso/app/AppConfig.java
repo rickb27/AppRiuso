9e41f5b631cb59cb229ef6f0eb5c7abccc5b431e
rickb27 <riccardobettiga.bet@gmail.com>
2017-04-17 11:45:33 +0200

    First Issue
    Issue of 2017-04-17

---
+++ app/src/main/java/ms_br/appriuso/app/AppConfig.java
@@ -0,0 +1,17 @@
+package ms_br.appriuso.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://appriuso.altervista.org/android_login_api/login.php";
+
+
+	// Server user register url
+	public static String URL_REGISTER = "http://appriuso.altervista.org/android_login_api/register.php";
+
+
+	// Server user login url
+	//public static String URL_LOGIN = "http://192.168.0.2/android_login_api/login.php";
+
+	// Server user register url
+	//public static String URL_REGISTER = "ttp://192.168.0.2/android_login_api/register.php";
+}
